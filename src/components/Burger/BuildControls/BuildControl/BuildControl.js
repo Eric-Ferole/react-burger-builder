a23afc020e51f722a7c040949c1716f340c9724f
Eric Ferole <eferole@pixelcircus.ca>
2020-06-08 23:39:25 -0400

    Adding buy now button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,9 +5,10 @@ import classes from './BuildControl.css'
 
 const buildControl = (props) => (
   <div>
-    <div className={classes.BuildControl}>{props.label}</div>
+    <div className={classes.BuildControl}>{props.label}
     <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
     <button className={classes.More} onClick={props.added}>More</button>
+    </div>
   </div>
 );
 
