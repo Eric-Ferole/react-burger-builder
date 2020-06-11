e790d9b4c787b083240f31fd19852397361c9a6a
Eric Ferole <eferole@pixelcircus.ca>
2020-06-11 08:29:29 -0400

    adding and changes on toolbar

--- src/components/Navigation/Toobar/Toolbar.js
+++ src/components/Navigation/Toobar/Toolbar.js
@@ -3,10 +3,13 @@ import React from 'react';
 // Style 
 import classes from './Toolbar.css'
 
+// Components
+import Logo from '../../Logo/Logo';
+
 const toolbar = ( props ) => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>
       ...
     </nav>
