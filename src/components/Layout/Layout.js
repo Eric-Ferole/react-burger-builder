dc63277425c852852c3fc9e7472d0ebc16ee33d8
Eric Ferole <eferole@pixelcircus.ca>
2020-06-10 23:41:52 -0400

    Adding menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import classes from './Layout.css';
-
+import Toolbar from '../Navigation/Toobar/Toolbar';
 import Aux from '../../hoc/Aux';
 
 const layout = ( props ) => (
   <Aux>
-    <div>Toolbar, Sidedrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>
