529f37ed57aaf8df489bd2bfd199d5e21855965b
Eric Ferole <eferole@pixelcircus.ca>
2020-06-12 15:39:52 -0400

    Adding lifeclycle methods

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,11 +4,10 @@ import React from 'react';
 import classes from './BuildControl.css'
 
 const buildControl = (props) => (
-  <div>
-    <div className={classes.BuildControl}>{props.label}
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
     <button className={classes.More} onClick={props.added}>More</button>
-    </div>
   </div>
 );
 
