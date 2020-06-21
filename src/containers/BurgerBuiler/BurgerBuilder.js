10b182b215523d14b20296b557e30c5106e647ac
Eric Ferole <eferole@pixelcircus.ca>
2020-06-21 12:55:07 -0400

    Adding navigation to checkout page

--- src/containers/BurgerBuiler/BurgerBuilder.js
+++ src/containers/BurgerBuiler/BurgerBuilder.js
@@ -90,28 +90,29 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState( { loading: true } );
-    const order = {
-      ingredient: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Éric',
-        address: {
-          street: 'Hutchison',
-          zipCode: 'H3N 2Z1',
-          country: 'Canada'
-        },
-        email: 'eferole@pixelcircus.ca'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState( { loading: false, purchasing: false } )
-      })
-      .catch(error => {
-        this.setState( { loading: false, purchasing: false } )
-      });
+    // this.setState( { loading: true } );
+    // const order = {
+    //   ingredient: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Éric',
+    //     address: {
+    //       street: 'Hutchison',
+    //       zipCode: 'H3N 2Z1',
+    //       country: 'Canada'
+    //     },
+    //     email: 'eferole@pixelcircus.ca'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState( { loading: false, purchasing: false } )
+    //   })
+    //   .catch(error => {
+    //     this.setState( { loading: false, purchasing: false } )
+    //   });
+    this.props.history.push('/checkout');
   }
 
   render() {
