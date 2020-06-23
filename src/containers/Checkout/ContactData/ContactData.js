22f3c277844820c804563974f2a5250d22771557
Eric Ferole <eferole@pixelcircus.ca>
2020-06-22 22:01:08 -0400

    Adding contact data and finishing order submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,32 +1,88 @@
-import React, { Component } from 'react';
-import Button from '../../../components/UI/Button/Button';
-import classes from './ContactData.css';
-
+import React, { Component } from "react";
+import Button from "../../../components/UI/Button/Button";
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from "./ContactData.css";
+import axios from '../../../axios.order';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
+    name: "",
+    email: "",
     address: {
-      street: '',
-      postalCode: ''
-    }
-  }
+      street: "",
+      postalCode: "",
+    },
+    loading: false
+  };
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredient: this.state.ingredients,
+      price: this.props.price,
+      customer: {
+        name: "Fucker",
+        address: {
+          street: "Hutchison",
+          zipCode: "H3N 2Z1",
+          country: "Canada",
+        },
+        email: "eferole@pixelcircus.ca",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => {
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+      });
+  };
+
   render() {
+    let form = (<form action="">
+    <input
+      className={classes.Input}
+      type="text"
+      name="name"
+      placeholder="Your Name"
+    />
+    <input
+      className={classes.Input}
+      type="text"
+      name="email"
+      placeholder="Your Email"
+    />
+    <input
+      className={classes.Input}
+      type="text"
+      name="street"
+      placeholder="Street"
+    />
+    <input
+      className={classes.Input}
+      type="text"
+      name="postal"
+      placeholder="Postal Code"
+    />
+    <Button btnType="Success" clicked={this.orderHandler}>
+      Order
+    </Button>
+  </form>);
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your contact data</h4>
-        <form action="">
-          <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-          <input className={classes.Input} type="text" name="email" placeholder="Your Email"/>
-          <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-          <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
-          <Button btnType="Success">Order</Button>
-        </form>
+        {form}
       </div>
-    )
+    );
   }
 }
-
 
 export default ContactData;
