e896056397a2237498fb380155cb959fd3dfbc1e
Eric Ferole <eferole@pixelcircus.ca>
2020-06-24 14:04:12 -0400

    Adding dropdown to contact form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,8 +6,8 @@ import classes from "./Input.css";
 const input = (props) => {
   let inputElement = null;
 
-  switch (props.inputtype) {
-    case "input":
+  switch (props.elementType) {
+    case ("input"):
       inputElement = (
         <input
           className={classes.InputElement}
@@ -16,13 +16,26 @@ const input = (props) => {
         />
       );
       break;
-    case "textarea":
+    case ("textarea"):
       inputElement = (
         <textarea
           className={classes.InputElement}
           {...props.elementConfig}
           value={props.value}
         />
+      );
+      break;
+    case ("select"):
+      inputElement = (
+        <select
+          className={classes.InputElement}
+          value={props.value}>
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+        ))}
+        </select>
       );
       break;
     default:
